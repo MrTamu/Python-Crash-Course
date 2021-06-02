055af5721ce2657062dbbadefbd3aaf4c4e8f7da
MrTamu <amitgurung487@gmail.com>
2021-06-02 19:10:23 +0545

    Add files via upload

---
+++ Chapter_2_Variables_and_simple_data_types/favorite_number.py
@@ -0,0 +1,6 @@
+# 2-9. Favorite Number: Use a variable to represent your favorite number. 
+# Then, using that variable, create a message that reveals your favorite number. Print that message.
+
+fav_num=3
+message="My favorite number is " + str(fav_num)
+print(message)
