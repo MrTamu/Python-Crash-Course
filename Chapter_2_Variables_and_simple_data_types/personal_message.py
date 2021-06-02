82768c74274061f072853ecf98cba58fa71a1195
MrTamu <amitgurung487@gmail.com>
2021-06-02 19:09:17 +0545

    Create personal_message.py

---
+++ Chapter_2_Variables_and_simple_data_types/personal_message.py
@@ -0,0 +1,9 @@
+# 2-3. Personal Message: Use a variable to represent a person’s name, and print a message to that person. 
+# Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+
+name="eric"
+print(f'"Hello {name.title()}, would you like to learn some Python today?"')
+print(f"Hello {name.title()}, would you like to learn some Python today?")
+
+# Note = You can use f-string format in two ways single quote & double quote to avoid syntax error
+# Use f-string single quote format if you want whole output inside double quote 
